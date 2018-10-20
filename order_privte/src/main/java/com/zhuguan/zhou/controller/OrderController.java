03eb3b15dfec747533226b7f232cf5a78341da89
zhoushisheng <1358281371@qq.com>
2018-10-20 15:34:36 +0800

    订单服务

--- order_privte/src/main/java/com/zhuguan/zhou/controller/OrderController.java
+++ order_privte/src/main/java/com/zhuguan/zhou/controller/OrderController.java
@@ -4,6 +4,8 @@ package com.zhuguan.zhou.controller;
 import com.zhuguan.com.api.OrderApi;
 import com.zhuguan.com.model.OrderDto;
 import com.zhuguan.zhou.response.ResponseData;
+import com.zhuguan.zhou.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +15,23 @@ import java.util.List;
 @RestController
 public class OrderController implements OrderApi {
 
+    @Autowired
+    private OrderService orderService;
+
 
     @Override
     public ResponseData<OrderDto> getOrderDtoInfoById(@RequestParam("id") String id) {
-        return null;
+        ResponseData<OrderDto> resp = new ResponseData<>();
+        OrderDto orderDto = orderService.getgetOrderDtoInfoById(id);
+        resp.setData(orderDto);
+        return resp;
     }
 
     @Override
     public ResponseData<List<OrderDto>> getOrderDtoInfoList(@RequestBody OrderDto orderDto) {
-        return null;
+        List<OrderDto> orderDtoInfoList = orderService.getOrderDtoInfoList();
+        ResponseData<List<OrderDto>> resp = new ResponseData<>();
+        resp.setData(orderDtoInfoList).ok();
+        return resp;
     }
 }
--- order_privte/src/main/java/com/zhuguan/zhou/service/impl/OrderServiceImpl.java
+++ order_privte/src/main/java/com/zhuguan/zhou/service/impl/OrderServiceImpl.java
@@ -1,12 +1,19 @@
 package com.zhuguan.zhou.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.zhuguan.com.model.OrderDto;
+import com.zhuguan.zhou.ProductClient;
+import com.zhuguan.zhou.model.ProductDto;
+import com.zhuguan.zhou.response.ResponseData;
 import com.zhuguan.zhou.service.OrderService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -14,13 +21,36 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private ProductClient productClient;
+
     @Override
-    public OrderDto getgetOrderDtoInfoById(String id) {
-        return null;
+    public OrderDto getgetOrderDtoInfoById(String id)  {
+        String url = "http://product-paren";
+        url = url + "/product/getProductInfoById?" + "id=" + id;
+        ResponseData resp = restTemplate.getForObject(url, ResponseData.class);
+        System.out.println(JSON.toJSONString(resp.getData()));
+        OrderDto orderDto = new OrderDto();
+        System.out.println("String:" + resp.getData().toString());
+        System.out.println("JSON:" + JSON.toJSONString(resp.getData()));
+        orderDto.setProductDto(JSON.parseObject(JSON.toJSONString(resp.getData()), ProductDto.class));
+        orderDto.setOrderName("第一次订单");
+        return orderDto;
     }
 
     @Override
     public List<OrderDto> getOrderDtoInfoList() {
-        return null;
+        ResponseData<List<ProductDto>> listResponseData = productClient.getgetProductInfoList();
+        System.out.println("JSON:" +JSON.toJSONString(listResponseData));
+        List<OrderDto> orderDtoList = new ArrayList<>();
+        listResponseData.getData().forEach(t->{
+            int i = 0;
+            OrderDto orderDto = new OrderDto();
+            orderDto.setProductDto(t);
+            orderDto.setId(UUID.randomUUID().toString());
+            orderDto.setOrderName("生成订单" + i++);
+            orderDtoList.add(orderDto);
+        });
+        return orderDtoList;
     }
 }
