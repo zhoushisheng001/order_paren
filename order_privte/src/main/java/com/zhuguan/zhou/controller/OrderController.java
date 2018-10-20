4af8b958fb8ae4cbe2f54c8d26cdf8e433312a28
zhoushisheng <1358281371@qq.com>
2018-10-21 00:19:25 +0800

    订单服务12

--- order_privte/src/main/java/com/zhuguan/zhou/controller/OrderController.java
+++ order_privte/src/main/java/com/zhuguan/zhou/controller/OrderController.java
@@ -6,19 +6,34 @@ import com.zhuguan.com.api.OrderApi;
 import com.zhuguan.com.model.OrderDto;
 import com.zhuguan.zhou.response.ResponseData;
 import com.zhuguan.zhou.service.OrderService;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class OrderController implements OrderApi {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    ExecutorService executorService = Executors.newFixedThreadPool(6);
+
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
 
 
     @Override
@@ -38,9 +53,30 @@ public class OrderController implements OrderApi {
         return resp;
     }
 
-    public ResponseData<List<OrderDto>> getOrderDtoInfoListInfo(@RequestBody OrderDto orderDto){
+    /**
+     *
+       这里面是服务降级，但是对应的降级服务要和上面的
+       fallbackMethod = "getOrderDtoInfoListInfo"
+      的方法名称和入参数一样
+     */
+   public ResponseData<List<OrderDto>> getOrderDtoInfoListInfo(@RequestBody OrderDto orderDto){
         List<OrderDto> orderDtoInfoList = new ArrayList<>();
         ResponseData<List<OrderDto>> resp = new ResponseData<>();
+        executorService.execute(new Runnable() {
+           @Override
+           public void run() {
+               String sendValue = "getOrderDtoInfo";
+               String sendInfo = redisTemplate.opsForValue().get(sendValue);
+               logger.info("=========》获得信息为：" + sendInfo);
+               if (StringUtils.isBlank(sendInfo)) {
+                   logger.info("发送消息服务已经挂了...");
+                   redisTemplate.opsForValue().set(sendValue,"服务已经挂了",100,TimeUnit.SECONDS);
+               } else {
+                   logger.info("短信已经发送过了...");
+               }
+           }
+       });
+        logger.info("OK.....");
         resp.setCode(-1);
         resp.setMsg("下单人数太多吃不消....");
         resp.setData(orderDtoInfoList);
